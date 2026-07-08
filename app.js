3297dcb7f008498fee9eb4abba22c4b0bc65bc03
Tyler <tylerkc9513@gmail.com>
2026-07-07 20:39:48 -0500

    Corrected line 11 of app.js (swapped "string" and "substring" in console.log)

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ console.log("\nReverse the string \"ABCDEFG\": " + stringUtils.reverse("ABCDEFG"
 console.log("Reverse the number 123: " + stringUtils.reverse(123))
 console.log("reverse function with no argument: " + stringUtils.reverse())
 
-console.log("\nCheck if the string \"string\" contains the substring \"substring\": " + stringUtils.contains("substring","string"))
-console.log("Check if the string \"string\" contains the substring \"number\": " + stringUtils.contains("substring","number"))
+console.log("\nCheck if the string \"substring\" contains the substring \"string\": " + stringUtils.contains("substring","string"))
+console.log("Check if the string \"substring\" contains the substring \"number\": " + stringUtils.contains("substring","number"))
 console.log("Check if the number 123 contains the number 2: " + stringUtils.contains(123,2))
 console.log("contains function with no argument: " + stringUtils.contains())
